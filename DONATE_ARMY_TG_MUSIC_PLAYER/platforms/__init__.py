e3a4df7fdb1953485d35ca5eaa6be2e7de80c799
𝘿𝙊𝙉𝘼𝙏𝙀 ๛ 𝘼𝙍𝙈𝙔 🇮🇳 ™ <sanjoymandal.902137@gmail.com>
2024-11-20 15:28:53 +0530

    𝗗𝗢𝗡𝗔𝗧𝗘_𝗔𝗥𝗠𝗬 ✘ 𝗠𝗨𝗦𝗜𝗖

---
+++ DONATE_ARMY_TG_MUSIC_PLAYER/platforms/__init__.py
@@ -0,0 +1,16 @@
+#
+# Copyright (C) 2024 by DONATE_ARMY™@Github, < https://github.com/DONATE-ARMY-BOTS >.
+#
+# This file is part of < https://github.com/DONATE-ARMY-BOTS/DONATE_ARMY_TG_MUSIC_PLAYER > project,
+# and is released under the MIT License.
+# Please see < https://github.com/DONATE-ARMY-BOTS/DONATE_ARMY_TG_MUSIC_PLAYER/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from .Apple import AppleAPI
+from .Carbon import CarbonAPI
+from .Resso import RessoAPI
+from .Soundcloud import SoundAPI
+from .Spotify import SpotifyAPI
+from .Telegram import TeleAPI
+from .Youtube import YouTubeAPI
