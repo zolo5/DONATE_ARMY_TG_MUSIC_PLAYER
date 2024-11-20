951d7cef55e57ed75e1a111c010d6fc02f2a19c9
𝘿𝙊𝙉𝘼𝙏𝙀 ๛ 𝘼𝙍𝙈𝙔 🇮🇳 ™ <sanjoymandal.902137@gmail.com>
2024-11-20 23:59:41 +0530

    𝗗𝗢𝗡𝗔𝗧𝗘_𝗔𝗥𝗠𝗬 ✘ 𝗠𝗨𝗦𝗜𝗖

--- config/config.py
+++ config/config.py
@@ -5,7 +5,7 @@
 # and is released under the MIT License.
 # Please see < https://github.com/DONATE-ARMY-BOTS/DONATE_ARMY_TG_MUSIC_PLAYER/blob/master/LICENSE >
 #
-# All rights reserved.
+# All rights reserved.helo
 import os
 import re
 from os import getenv
