7612f0f6178ad52ff41c54e8ee69d08923f8890e
𝘿𝙊𝙉𝘼𝙏𝙀 ๛ 𝘼𝙍𝙈𝙔 🇮🇳 ™ <sanjoymandal.902137@gmail.com>
2024-11-20 15:14:40 +0530

    𝗗𝗢𝗡𝗔𝗧𝗘_𝗔𝗥𝗠𝗬 ✘ 𝗠𝗨𝗦𝗜𝗖

--- DONATE_ARMY_TG_MUSIC_PLAYER/plugins/tools/ping.py
+++ DONATE_ARMY_TG_MUSIC_PLAYER/plugins/tools/ping.py
@@ -1,1 +1,45 @@
+#
+# Copyright (C) 2024 by DONATE_ARMY™@Github, < https://github.com/DONATE-ARMY-BOTS >.
+#
+# This file is part of < https://github.com/DONATE-ARMY-BOTS/DONATE_ARMY_TG_MUSIC_PLAYER > project,
+# and is released under the MIT License.
+# Please see < https://github.com/DONATE-ARMY-BOTS/DONATE_ARMY_TG_MUSIC_PLAYER/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+from datetime import datetime
 
+from pyrogram import filters
+from pyrogram.types import Message
+
+from config import BANNED_USERS, PING_IMG_URL
+from DONATE_ARMY_TG_MUSIC_PLAYER import app
+from DONATE_ARMY_TG_MUSIC_PLAYER.core.call import DONATE_ARMY
+from DONATE_ARMY_TG_MUSIC_PLAYER.utils import bot_sys_stats
+from DONATE_ARMY_TG_MUSIC_PLAYER.utils.decorators.language import language
+from DONATE_ARMY_TG_MUSIC_PLAYER.utils.inline import support_group_markup
+
+
+@app.on_message(filters.command(["ping", "alive"]) & ~BANNED_USERS)
+@language
+async def ping_com(client, message: Message, _):
+    response = await message.reply_photo(
+        photo=PING_IMG_URL,
+        caption=_["ping_1"].format(app.mention),
+    )
+    start = datetime.now()
+    pytgping = await DONATE_ARMY.ping()
+    UP, CPU, RAM, DISK = await bot_sys_stats()
+    resp = (datetime.now() - start).microseconds / 1000
+    await response.edit_text(
+        _["ping_2"].format(
+            resp,
+            app.mention,
+            UP,
+            RAM,
+            CPU,
+            DISK,
+            pytgping,
+        ),
+        reply_markup=support_group_markup(_),
+    )
